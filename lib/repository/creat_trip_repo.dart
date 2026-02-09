17200a06f9e31878af24517985c590b4c340e07f
abdullahsheikh6835-beep <abdullahsheikh6835@gmail.com>
2026-02-09 08:54:50 +0500

    'sfjgk'

--- lib/repository/creat_trip_repo.dart
+++ lib/repository/creat_trip_repo.dart
@@ -1,39 +1,40 @@
-// import 'dart:developer';
+// // import 'dart:developer';
 
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:turist_app/model/trip_model.dart';
+// // import 'package:cloud_firestore/cloud_firestore.dart';
+// // import 'package:firebase_auth/firebase_auth.dart';
+// // import 'package:turist_app/model/trip_model.dart';
 
-// class CreatTripRepo {
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
+// // class CreatTripRepo {
+// //   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+// //   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-//   Future<void> createTrip(Map<String, dynamic> tripData, String tripId) async {
-//     final user = _auth.currentUser;
+// //   Future<void> createTrip(Map<String, dynamic> tripData, String tripId) async {
+// //     final user = _auth.currentUser;
 
-//     if (user == null) {
-//       throw Exception("User not logged in");
-//     }
-//     final data = await _firestore.collection('signupuser').doc(user.uid).get();
-//     final userData = data.data();
-//     await _firestore.collection("creattripuser").doc(tripId).set({
-//       ...tripData,
-//       'userId': user.uid,
-//       'username': userData?['name'] ?? '',
-//       'createdAt': FieldValue.serverTimestamp(),
-//     });
-//   }
+// //     if (user == null) {
+// //       throw Exception("User not logged in");
+// //     }
+// //     final data = await _firestore.collection('signupuser').doc(user.uid).get();
+// //     final userData = data.data();
+// //     await _firestore.collection("creattripuser").doc(tripId).set({
+// //       ...tripData,
+// //       'userId': user.uid,
+// //       'username': userData?['name'] ?? '',
+// //       'createdAt': FieldValue.serverTimestamp(),
+// //     });
+// //   }
 
-//   Future<List<Trip?>> activeTripsGet() async {
-//     final data = await _firestore.collection("creattripuser").get();
+// //   Future<List<Trip?>> activeTripsGet() async {
+// //     final data = await _firestore.collection("creattripuser").get();
 
-//     return data.docs.map((doc) {
-//       final json = doc.data();
-//       log("our data is $json");
-//       return Trip.fromJson(json);
-//     }).toList();
-//   }
-// }
+// //     return data.docs.map((doc) {
+// //       final json = doc.data();
+// //       log("our data is $json");
+// //       return Trip.fromJson(json);
+// //     }).toList();
+// //   }
+// // }
+
 
 
 import 'dart:developer';
@@ -67,16 +68,19 @@ class CreatTripRepo {
   Future<List<Trip>> activeTripsGet() async {
     final snapshot = await _firestore.collection("creattripuser").get();
 
-    final trips = snapshot.docs.map((doc) {
-      final json = doc.data();
-      log("Trip data: $json");
-      try {
-        return Trip.fromJson(json);
-      } catch (e) {
-        log("Error parsing trip: $e");
-        return null; // skip if error
-      }
-    }).whereType<Trip>().toList(); // remove nulls
+    final trips = snapshot.docs
+        .map((doc) {
+          final json = doc.data();
+          log("Trip data: $json");
+          try {
+            return Trip.fromJson(json);
+          } catch (e) {
+            log("Error parsing trip: $e");
+            return null; // skip if error
+          }
+        })
+        .whereType<Trip>()
+        .toList(); // remove nulls
 
     return trips;
   }
